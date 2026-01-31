903354a915b915192735d9a45c20e33de0e8b508
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-01-31 01:42:21 +0100

    Add some functions and methods
    - Added utils/prettyPrint.js helper function.
    - Made buildTree in the Tree class work.
    - Refactored Node.js to use getters and setters.

--- classes/Node.js
+++ classes/Node.js
@@ -1,10 +1,30 @@
 export default class Node {
-    #data;
-    #left;
-    #right;
-    constructor(data ) {
-        this.#data = data;
-        this.#left = null;
-        this.#right = null;
-    }
+  #data;
+  #left;
+  #right;
+  constructor(data) {
+    this.#data = data;
+    this.#left = null;
+    this.#right = null;
+  }
+
+  get data() {
+    return this.#data;
+  }
+
+  get left() {
+    return this.#left;
+  }
+
+  get right() {
+    return this.#right;
+  }
+
+  set left(node) {
+    this.#left = node;
+  }
+
+  set right(node) {
+    this.#right = node;
+  }
 }
--- classes/Tree.js
+++ classes/Tree.js
@@ -1,13 +1,68 @@
+import Node from "./Node.js";
+import prettyPrint from "../utils/prettyPrint.js";
 
 export default class Tree {
-    #root;
-    constructor(arr) {
-        this.#root = this.#buildTree(arr);
+  #root;
+
+  constructor(arr) {
+    this.root = this.#buildTree(arr);
+
+    // console.log(this.root);
+  }
+
+  #merge(leftArr, rightArr) {
+    let [i, j] = [0, 0];
+    const result = [];
+
+    // Merge two sorted arrays
+    while (i < leftArr.length && j < rightArr.length) {
+      if (leftArr[i] < rightArr[j]) {
+        result.push(leftArr[i++]);
+      } else {
+        result.push(rightArr[j++]);
+      }
     }
-    #buildTree(arr) {
-        console.log(arr);
-    }
+
+    // Add remaining elements
+    while (i < leftArr.length) result.push(leftArr[i++]);
+    while (j < rightArr.length) result.push(rightArr[j++]);
+
+    return result;
+  }
+
+  #mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    const left = this.#mergeSort(arr.slice(0, mid));
+    const right = this.#mergeSort(arr.slice(mid));
+    return this.#merge(left, right);
+  }
+
+  #buildTree(arr) {
+    arr = this.#mergeSort([...new Set(arr)]); // Remove duplicate and sort array
+    return this.#sortedArrayToBSTRecur(arr, 0, arr.length - 1);
+  }
+
+  #sortedArrayToBSTRecur(arr, start, end) {
+    if (start > end) return null;
+
+    let mid = start + Math.floor((end - start) / 2);
+    let root = new Node(arr[mid]);
+    
+    // Divide from middle element
+    root.left = this.#sortedArrayToBSTRecur(arr, start, mid - 1);
+    root.right = this.#sortedArrayToBSTRecur(arr, mid + 1, end);
+    
+    return root;
+  }
 }
+
+// TEST
+const testArrOne = [1, 5, 8, 11, 13];
+const testArrTwo = [15, 20, 50, 80];
 
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const tree = new Tree(arr);
+
+prettyPrint(tree.root);
