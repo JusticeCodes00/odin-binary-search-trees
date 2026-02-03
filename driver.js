2c149c44009eb439f2e4c783cbf03f287a30198e
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-02-03 03:38:59 +0100

    Add spacing in driver.js

--- driver.js
+++ driver.js
@@ -1,11 +1,11 @@
 import Tree from "./classes/Tree.js";
 
-function getRandomArrNum(range = 100) {
-    const arr = [];
-    for (let i = 0; i <= range; i++) {
-        arr.push(Math.floor(Math.random() * range));
-    }
-    return arr;
+function getRandomArrNum(size) {
+  const arr = [];
+  for (let i = 0; i <= size; i++) {
+    arr.push(Math.floor(Math.random() * 100));
+  }
+  return arr;
 }
 
 // DRIVER
@@ -13,32 +13,47 @@ const tree = new Tree(getRandomArrNum(10));
 
 console.log(`Tree:`);
 tree.prettyPrint();
+
 console.log(`Balanced: ${tree.isBalanced()}`);
+
 console.log(`LevelOrder: `);
 tree.levelOrderForEach(console.log);
+
 console.log(`preOrder: `);
 tree.preOrderForEach(console.log);
+
 console.log(`inOrder: `);
 tree.inOrderForEach(console.log);
+
 console.log(`postOrder: `);
 tree.postOrderForEach(console.log);
+
 console.log(`Adding 100, 200, and 300: `);
 tree.insert(100);
 tree.insert(200);
 tree.insert(300);
+
 console.log(`Tree:`);
 tree.prettyPrint();
+
 console.log(`Balanced: ${tree.isBalanced()}`);
+
 console.log(`Calling rebalance()`);
 tree.rebalance();
+
 console.log(`Tree:`);
 tree.prettyPrint();
+
 console.log(`Balanced: ${tree.isBalanced()}`);
+
 console.log(`LevelOrder: `);
 tree.levelOrderForEach(console.log);
+
 console.log(`preOrder: `);
 tree.preOrderForEach(console.log);
+
 console.log(`inOrder: `);
 tree.inOrderForEach(console.log);
+
 console.log(`postOrder: `);
 tree.postOrderForEach(console.log);
